cd980b148779587cc67d622e150700a1046ec26a
Cem Özcan <uvtwy@student.kit.edu>
2019-12-03 15:39:10 +0100

    Implement methods for permutation feature importance calculation

--- metalfi/src/data/meta/importance/permutation.py
+++ metalfi/src/data/meta/importance/permutation.py
@@ -1,4 +1,7 @@
 from metalfi.src.data.meta.importance.featureimportance import FeatureImportance
+from rfpimp import *
+from sklearn import linear_model
+from sklearn.svm import SVC
 
 
 class PermutationImportance(FeatureImportance):
@@ -7,4 +10,46 @@ class PermutationImportance(FeatureImportance):
         super(PermutationImportance, self).__init__(dataset)
 
     def calculateScores(self):
-        return
+        reg = linear_model.LinearRegression()
+        rf = RandomForestClassifier(random_state=101)
+        svc = SVC(gamma='auto')
+
+        target = self._dataset.getTarget()
+
+        #TODO: Calc. importances for feature subsets that are multicolinear
+        self.cvPermutationImportance(reg, target)
+        self.permutationImportance(reg, target)
+
+        self.cvPermutationImportance(rf, target)
+        self.permutationImportance(rf, target)
+
+        self.cvPermutationImportance(svc, target)
+        self.permutationImportance(svc, target)
+
+    def cvPermutationImportance(self, model, target):
+        # TODO: Calc. importances on testset?
+        X = self._data_frame.drop(target, axis=1)
+        y = self._data_frame[target]
+
+        imp = cv_importances(model, X, y)
+        plot_importances(imp).view()
+
+    def permutationImportance(self, model, target, features):
+        # TODO: Calc. importances on testset?
+        X = self._data_frame.drop(target, axis=1)
+        y = self._data_frame[target]
+
+        model.fit(X, y)
+        imp = importances(model, X, y, features=features)
+        plot_importances(imp).view()
+
+    #TODO: Use
+    def permutationImportance(self, model, target):
+        # TODO: Calc. importances on testset?
+        X = self._data_frame.drop(target, axis=1)
+        y = self._data_frame[target]
+
+        model.fit(X, y)
+        imp = importances(model, X, y)
+        plot_importances(imp).view()
+
