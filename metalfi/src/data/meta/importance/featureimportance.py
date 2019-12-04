a660a0059e65ce62e5ddd40d0dfec810397afebd
Cem Özcan <uvtwy@student.kit.edu>
2019-12-04 10:53:27 +0100

    Implement eli5 permutation importance

--- metalfi/src/data/meta/importance/featureimportance.py
+++ metalfi/src/data/meta/importance/featureimportance.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 class FeatureImportance(ABC):
 
+    #TODO: Score metric
     def __init__(self, dataset):
         self._dataset = dataset
         self._data_frame = dataset.getDataFrame()
--- metalfi/src/data/meta/importance/permutation.py
+++ metalfi/src/data/meta/importance/permutation.py
@@ -2,6 +2,9 @@ from metalfi.src.data.meta.importance.featureimportance import FeatureImportance
 from rfpimp import *
 from sklearn import linear_model
 from sklearn.svm import SVC
+from eli5.sklearn import PermutationImportance as eli5PI
+
+import eli5
 
 
 class PermutationImportance(FeatureImportance):
@@ -16,15 +19,18 @@ class PermutationImportance(FeatureImportance):
 
         target = self._dataset.getTarget()
 
-        #TODO: Calc. importances for feature subsets that are multicollinear
+        # TODO: Calc. importances for feature subsets that are multicollinear
         self.cvPermutationImportance(reg, target)
         self.permutationImportance(reg, target)
+        self.eli5PermutationImportance(reg, target)
 
         self.cvPermutationImportance(rf, target)
         self.permutationImportance(rf, target)
+        self.eli5PermutationImportance(rf, target)
 
         self.cvPermutationImportance(svc, target)
         self.permutationImportance(svc, target)
+        self.eli5PermutationImportance(svc, target)
 
     def cvPermutationImportance(self, model, target):
         # TODO: Calc. importances on testset?
@@ -41,3 +47,13 @@ class PermutationImportance(FeatureImportance):
         model.fit(X, y)
         imp = importances(model, X, y)
         plot_importances(imp).view()
+
+    def eli5PermutationImportance(self, model, target):
+        X = self._data_frame.drop(target, axis=1)
+        y = self._data_frame[target]
+
+        model.fit(X, y)
+        imp = eli5PI(model, random_state=101, cv="prefit").fit(X, y)
+
+        print(eli5.format_as_text(eli5.explain_weights(imp)))
+        print(X.columns)
