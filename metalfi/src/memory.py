fe89b07f0ba645c49ff3f6b5eeff2aece829f11a
Cem Özcan <cem.oezcan@outlook.com>
2021-07-04 17:36:22 +0200

    Change data set selection for testing purposes.

--- metalfi/src/memory.py
+++ metalfi/src/memory.py
@@ -63,7 +63,7 @@ class Memory:
             data = data[data['NumberOfFeatures'] > 20]
             space = "_comp_"
         else:
-            data = data[data['NumberOfInstances'] < 101]
+            data = data[data['NumberOfInstances'] < 501]
             data = data[data['NumberOfFeatures'] < 11]
             data = data[data['NumberOfFeatures'] > 4]
         data = data[data['NumberOfClasses'] == 2]
