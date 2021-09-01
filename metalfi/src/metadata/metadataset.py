254d299d6bf409e9bc5ae2fe6c4f20340c11c098
Jakob Bach <jakob.bach@kit.edu>
2021-09-01 15:22:18 +0200

    Add more type hints to MetaDataset
    Barely existed before. Addresses issue #45.

--- metalfi/src/metadata/metadataset.py
+++ metalfi/src/metadata/metadataset.py
@@ -1,30 +1,34 @@
+from typing import List, Tuple
+
 import pandas as pd
+
+from metalfi.src.metadata.dataset import Dataset
 
 
 class MetaDataset:
 
-    def __init__(self, dataset, train=False):
+    def __init__(self, dataset: Tuple[pd.DataFrame, str], train: bool = False):
         self.__name = dataset[1]
         self.__meta_data, self.__target_names, self.__times, self.nr_feat, self.nr_inst = \
             self.calculate_training_data(dataset[0]) if train else self.calculate_test_data(dataset[0])
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def get_meta_data(self):
+    def get_meta_data(self) -> pd.DataFrame:
         return self.__meta_data
 
-    def get_target_names(self):
+    def get_target_names(self) -> List[str]:
         return self.__target_names
 
-    def get_times(self):
+    def get_times(self) -> Tuple[float, float]:
         return self.__times
 
-    def get_nrs(self):
+    def get_nrs(self) -> Tuple[int, int]:
         return self.nr_feat, self.nr_inst
 
     @staticmethod
-    def calculate_training_data(dataset):
+    def calculate_training_data(dataset: Dataset) -> Tuple[pd.DataFrame, List[str], Tuple[float, float], int, int]:
         data_frames = list()
         nr_feat = 0
         nr_inst = 0
@@ -38,7 +42,7 @@ class MetaDataset:
         return pd.concat(data_frames), targets, times, nr_feat, nr_inst
 
     @staticmethod
-    def calculate_test_data(datasets):
+    def calculate_test_data(datasets: List[Dataset]) -> Tuple[pd.DataFrame, None, None, None, None]:
         data_frames = list()
         for dataset in datasets:
             data_frames.append(dataset.test_meta_data())
